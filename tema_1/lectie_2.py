19c7bad3a9421f8f629439b6fbcc2a54d98f3e93
anitlo <oltinaioana@yahoo.com>
2025-08-07 19:03:27 +0200

    added tema 1

---
+++ tema_1/lectie_2.py
@@ -0,0 +1,24 @@
+print('Hello world!') 
+print ('AI')
+print('caine')
+print('oras favorit \t = Maastricht') #\t = tab (spatiu)
+print ('culoare favorita \n =albastru') #\n = newline
+print ('\t Poezia preferata \n\n Catelus cu parul cret \n Fura rata din cotet \n Si se jura ce nu fura \n Dar l-am prins cu rata-n gura \n')
+
+#variable and string
+nume = 'oltina'
+print (nume)
+
+#int 
+varsta = 22
+
+#float 
+#ore_somn = 7.29 
+input_ore_somn = input('Cat ai dormit aseara?') #ce este rezultatul pus de mine in temrinal? adica 2??
+ore_somn = float(input_ore_somn)
+
+#bool - boolean
+odihnit = True
+
+print('nume:', nume, 'varsta:', varsta, 'ore dormite:', ore_somn, 'odihna:', odihnit, 'input:' , input_ore_somn)
+print(type(input_ore_somn)) #rezulattul a fost pus ca string in acest context
