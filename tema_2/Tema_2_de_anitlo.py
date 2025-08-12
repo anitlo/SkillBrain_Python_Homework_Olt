3b79a5d0d5c721e0d9af4a7cb115c98efd84a3bc
anitlo <oltinaioana@yahoo.com>
2025-08-12 18:59:33 +0200

    update

--- tema_2/Tema_2_de_anitlo.py
+++ tema_2/Tema_2_de_anitlo.py
@@ -1,4 +1,4 @@
-# #calculator
+# #calculator a doua numere de la un utilizato
 
 x = float(input('un nr intre 1 si 100 ='))
 y = float(input('un numar intre 100 si 200 ='))
@@ -12,31 +12,35 @@ print("restul =", y % x)
 
 # #cum calculam perimetrul si aria dreptunghiului cu formula
 lungime = float(input("Lungime: "))
-latime = float(input("latime"))
+latime = float(input("latime:"))
 perimetru = 2 * (lungime + latime)
 aria = lungime * latime
 print(perimetru)
 print(aria)
 
-# #compara numere
+# #compara numere - Cere două numere de la utilizator
+#Afișează dacă primul este mai mare, mai mic sau egal cu al doilea (3 printuri)
+
 nr_unu = float(input('da un numar ='))
 nr_doi = float (input ('da al doilea numar ='))
-print(bool(nr_unu>nr_doi))
-print(bool(nr_unu<nr_doi))
-print(bool(nr_unu>=nr_doi))
+print('este mai mare 1 decat 2?', bool(nr_unu>nr_doi))
+print('este numarul unu mai mare decat numarul doi?', bool(nr_unu<nr_doi))
+print('este numarul 1 mai mare sau egal decat numarul doi?', bool(nr_unu>=nr_doi))
 
 # #ce varsta ai
 varsta_ta = float(input('Anul nasterii?'))
 an_actual = 2025
 print(float(an_actual-varsta_ta))
 
-#par impar
+#par impar - Cere un număr de la utilizator + Afișează dacă este par sau impar
+#daca este restul 1 este impar, daca este 0 este par - cand impart cu doi
+
 numarul_tau = int(input('Spune un numar'))
 rest = numarul_tau % 2
 verificare_paritate = print('Este impar?', bool(rest))
 
 
-#ultimul exercitiu
+#ultimul exercitiu - Cere de la utilizator un număr de secunde și afișează: Câte ore, minute și secunde reprezintă
 a=int(input('Adauga nr secunde:'))
 ore=a//3600
 rest_secunde=a%3600
